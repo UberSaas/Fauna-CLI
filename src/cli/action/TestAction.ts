502c9c2d5e6178e05e0fec9f614264036786765d
Frank Weijers <info@frankweijers.me>
2020-12-18 00:01:20 +0100

    Add enquirer for prompting

--- src/cli/action/TestAction.ts
+++ src/cli/action/TestAction.ts
@@ -2,9 +2,21 @@ import * as chalk from 'chalk'
 import { Input } from '@nestjs/cli/commands'
 import { AbstractAction } from '@nestjs/cli/actions'
 import * as ora from 'ora'
+import { prompt } from 'enquirer'
 
 export class TestAction extends AbstractAction {
-  public handle(inputs: Input[], options: Input[]): Promise<void> {
+  public async handle(inputs: Input[], options: Input[]): Promise<void> {
+    const response: any = await prompt({
+      type: 'confirm',
+      initial: false,
+      name: 'confirmed',
+      message: `Want to perform ${chalk.yellowBright('Test action')}?`,
+    })
+
+    if (!response.confirmed) {
+      process.exit(0)
+    }
+
     const spinner = ora().start('Test action started')
 
     return new Promise((resolve) => {
