57e839fda56da3362da31bec5a263d7831aa01b6
Frank Weijers <info@frankweijers.me>
2020-12-17 23:29:37 +0100

    Add ora spinner

--- src/cli/action/TestAction.ts
+++ src/cli/action/TestAction.ts
@@ -1,19 +1,20 @@
 import * as chalk from 'chalk'
 import { Input } from '@nestjs/cli/commands'
 import { AbstractAction } from '@nestjs/cli/actions'
+import * as ora from 'ora'
 
 export class TestAction extends AbstractAction {
   public handle(inputs: Input[], options: Input[]): Promise<void> {
-    console.info(chalk.green('Test action started'))
+    const spinner = ora().start('Test action started')
 
     return new Promise((resolve) => {
-      setTimeout(resolve, 1000)
+      setTimeout(resolve, 5000)
     })
       .then(() => {
-        console.info(chalk.green('Action completed'))
+        spinner.succeed(chalk.green('Action completed'))
       })
       .catch((error) => {
-        console.error('Action failed')
+        spinner.fail(chalk.red('Action failed'))
         console.error(error)
       })
   }
